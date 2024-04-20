a00e39cf4d7f25d546998069c7212fcdaf565446
R. Aspeslag <ruben.aspeslag@student.howest.be>
2024-04-20 16:46:51 +0200

    DAL init AddCard method

--- Howest.MagicCards.DAL/Models/Deck.cs
+++ Howest.MagicCards.DAL/Models/Deck.cs
@@ -15,6 +15,28 @@ namespace Howest.MagicCards.DAL.Models
         public int Id { get; set; }
         public string DeckName { get; set; }
 
-        public virtual ICollection<int> CardIds { get; set; }
+        public virtual ICollection<CardDeck> CardDecks { get; set; }
+
+        internal void AddCard(Card card)
+        {
+            bool found = false;
+            foreach (var deckCard in CardDecks) 
+            {
+                if (deckCard.CardId == card.Id)
+                {
+                    found = true;
+                    deckCard.Amount++;
+                }   
+            }
+
+            if (!found)
+            {
+                CardDecks.Add(new CardDeck() {
+                    CardId = card.Id,
+                    DeckId = this.Id,
+                    Amount = 1
+                });
+            }
+        }
     }
 }
---
+++ Howest.MagicCards.DAL/Repositories/JsonDeckReposetory.cs
@@ -0,0 +1,141 @@
+﻿using Howest.MagicCards.DAL.Json;
+using Howest.MagicCards.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories
+{
+    internal class JsonDeckReposetory : IDeckRepository
+    {
+        public List<Deck> Decks { get; set; }
+        public DeckRepo()
+        {
+            Decks = getDecks();
+        }
+
+        public List<Deck> getDecks()
+        {
+
+            return  new JsonRepo().getDecks().ToList();
+       
+        }
+
+        public Deck getDeck(long id)
+        {
+            foreach (var deck in Decks)
+            {
+                if (deck.Id == id)
+                {
+                    return deck;
+                }
+            }
+            throw new Exception("deck not fount");
+        }
+
+        public void saveDecks(List<Deck> decks)
+        {
+            new JsonRepo().save(decks);
+        }
+
+        public void saveDeck(long id, Deck deck)
+        {
+            
+            new JsonRepo().save(id, deck);
+        }
+
+
+
+        public void AddDeck(Deck deck)
+        {
+            Decks.Add(deck);
+            saveDecks(Decks);
+        }
+
+        public void RemoveDeck(long id)
+        {
+            Decks.Remove(getDeck(id));
+            saveDecks(Decks);
+        }
+
+        public void Clear()
+        {
+            Decks = new List<Deck>();
+            saveDecks(new List<Deck>());
+        }
+
+        public long CreateDeck(string name)
+        {
+            int id = getDeckCount();
+            Deck deck = new Deck() { Id = id, DeckName = name };
+            this.AddDeck(deck);
+            saveDeck(id, deck);
+            return id;
+        }
+
+        public int getDeckCount()
+        {
+            return Decks.Count();
+        }
+
+
+
+        public void AddCardToDeck(int deckId, Card card)
+        {
+            Deck deck = getDeck(deckId);
+            deck.AddCard(card);
+            saveDeck(deckId, deck);
+        }
+
+        public void AddCardToDeck(int deckId, int cardId)
+        {
+            Deck deck = getDeck(deckId);
+            deck.AddCard(new CardRepo().GetCard(cardId));
+            saveDeck(deckId, deck);
+        }
+
+        public void RemoveCardFromDeck(int deckId, int cardId)
+        {
+            Deck deck = getDeck(deckId);
+            deck.RemoveCard(cardId);
+            saveDeck(deckId, deck);
+        }
+
+        public void RemoveCardFromDeck(int deckId, long cardId)
+        {
+            Deck deck = getDeck(deckId);
+            Card card = deck.GetCard(cardId);
+            deck.RemoveCard(card);
+            saveDeck(deckId, deck);
+        }
+
+        public void ClearDeck(int deckId)
+        {
+            Deck deck = getDeck(deckId);
+            deck.Clear();
+            saveDeck(deckId, deck);
+        }
+
+        public void ClearAllDecks()
+        {
+            Clear();
+            saveDecks(new List<Deck>());
+        }
+
+        public void UbdateCardsOfDeck(int id, DeckCardsId cards)
+        {
+            Deck deck = getDeck(id);
+            deck.Clear();
+            foreach (int card in cards.Cards)
+            {
+                deck.AddCard(card);
+            }
+            saveDecks(Decks);
+        }
+
+
+    }
+}
+
