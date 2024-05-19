c1116de8f74625e66f81f88cb992579fa4caacf2
R. Aspeslag <ruben.aspeslag@student.howest.be>
2024-05-19 21:17:21 +0200

    make CardDetailDTO

---
+++ Howest.MagicCards.Shared/DTO/CardDetailDTO.cs
@@ -0,0 +1,72 @@
+﻿using Howest.MagicCards.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.Shared.DTO
+{
+    public class CardDetailDTO
+    {
+    public partial class Card
+    {
+        public long Id { get; init; }
+
+        public string Name { get; init; }
+
+        public string ManaCost { get; init; }
+
+        public string ConvertedManaCost { get; init; }
+
+        public string Type { get; init; }
+
+        public string RarityCode { get; init; }
+
+        public string SetCode { get; init; }
+
+        public string Text { get; init; }
+
+        public string Flavor { get; init; }
+
+        public long? ArtistId { get; init; }
+
+        public string Number { get; init; }
+
+        public string Power { get; init; }
+
+        public string Toughness { get; init; }
+
+        public string Layout { get; init; }
+
+        public int? MultiverseId { get; init; }
+
+        public string OriginalImageUrl { get; init; }
+
+        public string Image { get; init; }
+
+        public string OriginalText { get; init; }
+
+        public string OriginalType { get; init; }
+
+        public string MtgId { get; init; }
+
+        public string Variations { get; init; }
+
+        public DateTime? CreatedAt { get; init; }
+
+        public DateTime? UpdatedAt { get; init; }
+
+        public virtual Artist Artist { get; init; }
+
+        public virtual ICollection<CardColor> CardColors { get; init; } = new List<CardColor>();
+
+        public virtual ICollection<CardType> CardTypes { get; init; } = new List<CardType>();
+
+        public virtual Rarity RarityCodeNavigation { get; init; }
+
+        public virtual Set SetCodeNavigation { get; init; }
+    }
+
+}
+}
